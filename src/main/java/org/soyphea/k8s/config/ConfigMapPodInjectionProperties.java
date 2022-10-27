87c66dd1b2048f0b767777eb104030abc9f6d2bc
aleja613 <47822224+aleja613@users.noreply.github.com>
2022-10-27 10:07:44 -0300

    Update ConfigMapPodInjectionProperties.java

--- src/main/java/org/soyphea/k8s/config/ConfigMapPodInjectionProperties.java
+++ src/main/java/org/soyphea/k8s/config/ConfigMapPodInjectionProperties.java
@@ -21,16 +21,12 @@ public class ConfigMapPodInjectionProperties {
     String password = "admin";
 }
 
-public static List<String> zipSlipNoncompliant(ZipFile zipFile) throws IOException { 
-    Enumeration<? extends ZipEntry> entries = zipFile.entries(); 
-    List<String> filesContent = new ArrayList<>();
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 
-    while (entries.hasMoreElements()) {
-      ZipEntry entry = entries.nextElement();
-      File file = new File(entry.getName());
-      String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8); // Noncompliant
-      filesContent.add(content);
-    }
+public void runUnsafe(HttpServletRequest request) throws IOException {
+  String cmd = request.getParameter("command");
+  String arg = request.getParameter("arg");
 
-    return filesContent;
+  Runtime.getRuntime().exec(cmd+" "+arg); // Noncompliant
 }
