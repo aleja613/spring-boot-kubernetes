40ffd21fe65607f2639a594475273a18ecc0bba3
aleja613 <47822224+aleja613@users.noreply.github.com>
2022-10-27 10:18:58 -0300

    Update ConfigMapPodInjectionProperties.java

--- src/main/java/org/soyphea/k8s/config/ConfigMapPodInjectionProperties.java
+++ src/main/java/org/soyphea/k8s/config/ConfigMapPodInjectionProperties.java
@@ -19,22 +19,8 @@ public class ConfigMapPodInjectionProperties {
     String user = "admin";
     
     String password = "admin";
+    
+    String query = "Select * from USER where lastname= " + name;
+        
 }
 
-@Autowired
-public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
-  auth.jdbcAuthentication()
-    .dataSource(dataSource)
-    .usersByUsernameQuery("SELECT * FROM users WHERE username = ?")
-    .passwordEncoder(new StandardPasswordEncoder()); // Noncompliant
-
-  // OR
-  auth.jdbcAuthentication()
-    .dataSource(dataSource)
-    .usersByUsernameQuery("SELECT * FROM users WHERE username = ?"); // Noncompliant; default uses plain-text
-
-  // OR
-  auth.userDetailsService(...); // Noncompliant; default uses plain-text
-  // OR
-  auth.userDetailsService(...).passwordEncoder(new StandardPasswordEncoder()); // Noncompliant
-}
