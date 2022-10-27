569717a2d0ff24f7323b6f6b616d5538dc173b51
aleja613 <47822224+aleja613@users.noreply.github.com>
2022-10-27 10:28:17 -0300

    Update test_vulnerabiity.java

--- src/main/java/org/soyphea/k8s/config/test_vulnerabiity.java
+++ src/main/java/org/soyphea/k8s/config/test_vulnerabiity.java
@@ -6,11 +6,8 @@ public class test {
 
     public static void main(String[] args) {
       try
-      {
-        
-        Cipher c19 = Cipher.getInstance("RC4"); // Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security)
-      
-       
+      {        
+        Cipher c19 = Cipher.getInstance("RC4"); // Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security)          
       }
       catch(NoSuchAlgorithmException|NoSuchPaddingException e)
       {
